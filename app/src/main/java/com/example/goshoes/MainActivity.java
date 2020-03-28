807ca046afe8029a2eb1fcf4d3bb7b507161be5b
YuliaSofi <yuliasofi92@gmail.com>
2020-03-28 13:39:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/goshoes/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.goshoes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickLogin(View view) {
+        Intent i = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
